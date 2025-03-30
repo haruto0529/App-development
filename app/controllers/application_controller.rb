cfcab0bd9edadb34d5e2595625f4e435d71f9fe1
福井陽登 <fukuiharuto@fukuiakiratounoMacBook-Air.local>
2025-03-30 20:04:26 +0900

    Added more sign-up options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   include Devise::Controllers::Helpers
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :gender, :introduce, :experience_history, :prefectures])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :gender, :introduce, :experience_history, :prefectures])
+  end
 end
