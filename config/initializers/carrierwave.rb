041a02d1a281dd1d26da293c7678ba7a11d431e3
福井陽登 <fukuiharuto@fukuiakiratounoMacBook-Air.local>
2025-04-04 18:27:49 +0900

    carrierwave.rb setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
-# CarrierWaveの設定呼び出し
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境の場合
+  if Rails.env.production?
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = ENV['AWS_BUCKET']
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      path_style: true
+    provider:              'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:                'ap-northeast-1',
     }
+    config.fog_directory  = ENV['AWS_BUCKET']
+  else
+    config.storage = :file
+    config.enable_processing = Rails.env.development?
   end
-  #日本語ファイル名の設定
+
   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
